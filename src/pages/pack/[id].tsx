b94d39b231cb7615d9e5bd9fee8746f869588426
Jordan Vera <verawebdevelopment@gmail.com>
2024-10-14 22:05:25 -0500

    added comment

--- src/pages/pack/[id].tsx
+++ src/pages/pack/[id].tsx
@@ -10,6 +10,7 @@ export default function PackPage() {
   const [pack, setPack] = useState<any>(null);
   const [loading, setLoading] = useState(true);
 
+  // asdasd
   const fetchPackData = async (packId: string) => {
     setLoading(true);
     try {
