227241521e39ac25754ab52fd1a1e8ca8c7f6c64
Jordan Vera <verawebdevelopment@gmail.com>
2024-10-21 23:43:58 -0500

    Working on small time widgets for pack creation.

--- src/components/create/NameYourPack.tsx
+++ src/components/create/NameYourPack.tsx
@@ -4,9 +4,35 @@ const NameYourPack = () => {
   const [name, setName] = useState('');
 
   return (
-    <div className="flex flex-col gap-5 rounded-lg bg-zinc-900 p-5">
-      NameYourPack
-    </div>
+    <section className="flex flex-col gap-5 rounded-lg bg-zinc-900 p-5">
+      <header className="flex  items-center gap-5">
+        <h2 className="flex items-center justify-center text-xl font-bold text-orange-500 bg-opacity-30 bg-orange-500 rounded-lg h-10 w-10">
+          3
+        </h2>
+
+        <h2 className="text-xl font-bold">Pack Name</h2>
+      </header>
+
+      <label className="flex flex-col gap-2">
+        <div className="flex justify-between items-center">
+          <p className="text-sm text-white font-semibold">Pack Name</p>
+          <p className="text-sm text-zinc-400 font-semibold">
+            (max 30 characters)
+          </p>
+        </div>
+
+        <input
+          type="text"
+          placeholder="Name your pack"
+          className="w-full p-2 rounded-md bg-zinc-800"
+        />
+      </label>
+
+      <p className="text-xs text-zinc-400 font-semibold">
+        Your pack name must not include any special characters, profanities, or
+        slurs.
+      </p>
+    </section>
   );
 };
 
--- src/components/create/SetCommision.tsx
+++ src/components/create/SetCommision.tsx
@@ -2,9 +2,31 @@ import { useState } from 'react';
 
 const SetCommision = () => {
   return (
-    <div className="flex flex-col gap-5 rounded-lg bg-zinc-900 p-5">
-      SetCommision
-    </div>
+    <section className="flex flex-col gap-5 rounded-lg bg-zinc-900 p-5">
+      <header className="flex  items-center gap-5">
+        <h2 className="flex items-center justify-center text-xl font-bold text-orange-500 bg-opacity-30 bg-orange-500 rounded-lg h-10 w-10">
+          4
+        </h2>
+
+        <h2 className="text-xl font-bold">Set Pack Commission</h2>
+      </header>
+
+      <label className="flex flex-col gap-2">
+        <div className="flex justify-between items-center">
+          <p className="text-sm text-white font-semibold">Commision</p>
+          <p className="text-sm text-zinc-400 font-semibold">(0.5% to 3.0%)</p>
+        </div>
+
+        <select className="w-full p-2 rounded-md bg-zinc-800" defaultValue="1">
+          <option value="0.5">0.5%</option>
+          <option value="1">1%</option>
+          <option value="1.5">1.5%</option>
+          <option value="2">2%</option>
+          <option value="2.5">2.5%</option>
+          <option value="3">3%</option>
+        </select>
+      </label>
+    </section>
   );
 };
 
