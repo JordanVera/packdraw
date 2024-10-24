cc69c856b0b6d49b1d30da642361ab2227bf6885
Jordan Vera <verawebdevelopment@gmail.com>
2024-10-24 15:05:08 -0500

    Working on choose pack img

--- src/components/create/ChoosePackImage.tsx
+++ src/components/create/ChoosePackImage.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 
 const ChoosePackImage = () => {
+  const [selectedImage, setSelectedImage] = useState<number | null>(null);
+
   return (
     <section className="flex flex-col gap-5 rounded-lg bg-zinc-900 p-5 flex-1">
       <header className="flex items-center gap-5">
@@ -9,18 +11,31 @@ const ChoosePackImage = () => {
         </h2>
 
         <h2 className="text-xl font-bold">Choose Pack Image</h2>
+      </header>
+      <div className="flex gap-5">
+        <div className="w-1/4">
+          <img
+            src={`/images/packs/version${selectedImage + 1}.webp`}
+            alt="Pack Image"
+          />
+        </div>
 
-        <div className="flex flex-wrap gap-5">
+        <div className="flex flex-wrap gap-5 w-3/4">
           {Array.from({ length: 14 }).map((_, index) => (
-            <div key={index} className="w-20  bg-zinc-800 rounded-lg">
+            <button
+              onClick={() => setSelectedImage(index)}
+              key={index}
+              className="w-20 rounded-lg"
+            >
               <img
                 src={`/images/packs/version${index + 1}.webp`}
                 alt="Pack Image"
+                className={`w-full object-cover`}
               />
-            </div>
+            </button>
           ))}
         </div>
-      </header>
+      </div>
     </section>
   );
 };
